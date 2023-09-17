54f0e48e63005c6e3ac33ef909202c7d1efa467b
DidasMba <daniel.mbarushi@gmail.com>
2023-09-17 18:46:23 +0300

    Completed assigment

--- index.js
+++ index.js
@@ -1,26 +1,12 @@
-function say(greeting, firstName ){
-    console.log(`${greeting}, ${firstName}`);
-}
-say("Goodbay", "Junior");
-
-function say(greeting, firstName){
-    console.log("firstName : ", firstName);
-    console.log("greeting :", greeting);
-    console.log(`${greeting},${firstName}`);
+function logTwoValues(value1, value2){
+    console.log(`The two value are ${value1}, ${value2}.`);
 }
 
-function add(x,y){
-    return x + y;
+function sayHelloTo(firstName){
+ console.log (`Hello, ${firstName}`);
 }
-console.log(add(1,2));
 
-function say(greeting, firstName){
-    return  `${greeting}, ${firstName}!`;
-}
-console.log(say("Hllo", "Didas"));
 
-function say(greeting, firstName){
-    return `${greeting}, ${firstName}!`;
-    console.log("i was called");
-
+function sayHelloTo(firstName = "User"){
+    console.log(`Hello, ${firstName});
 }
