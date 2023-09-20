fd4daf390b93f64d6c931ba2e20ee5a0971fe316
DidasMba <daniel.mbarushi@gmail.com>
2023-09-20 20:04:10 +0300

    control flow lab test

--- index.js
+++ index.js
@@ -36,10 +36,46 @@ function isDidas(Personne){
     }else if 
 }
 
-*/
+
 
 console.log(typeof 42);
 console.log(typeof 'moi');
+console.log(typeof true);
+console.log(typeof undeclaredVariable);
+
+function isRose(isDidas){
+    if(isDidas == ' NOM'){
+        return 'HI dIDAS '
+    }else if (typeof isDidas === 'string'){
+        return ' sorry pas the name'
+    }
+}
+
+console.log(isRose());
+
+const greeting = "Hello";
+let answer;
+answer = 42;
+console.log(answer)
+
+function (){
+    console.log
+}
+
+ const truthValue = 'THis is truthy';
+ const falseyValue =0;
+
+ expression1 && expression2;
+ 
+ 
+ !truthValue;*/
+
+ function getFee(isMember){
+    return isMember? '$2.00' : '$10.00';
+ }
+console.log(getFee(true));
+console.log(getFee(false));
+console.log(getFee(null));
 
 
 
